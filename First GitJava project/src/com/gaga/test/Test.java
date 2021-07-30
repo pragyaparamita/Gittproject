75a168b9df8b026fb9178b8ddf9bd74b63749e38
Teamlrader <Teamleader@gmail.com>
2021-07-30 12:56:25 +0530

    created by TL.

---
+++ First GitJava project/src/com/gaga/test/Test.java
@@ -0,0 +1,13 @@
+package com.gaga.test;
+
+import com.gaga.beans.utility;
+
+public class Test {
+	
+	public static void main(string[] args) {
+		utility u = new Utility();
+		System.out.println(u.getSystemDate());
+	}
+	
+
+}
